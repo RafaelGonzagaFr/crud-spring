b99c4558dd3d7f22fd5d28304ecc7a0db33c27de
Rafael Gonzaga <rafaelgonzag4@gmail.com>
2024-01-11 21:57:26 -0300

    1.2

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -3,11 +3,14 @@ package com.example.crud.controllers;
 import com.example.crud.domain.product.Product;
 import com.example.crud.domain.product.ProductRepository;
 import com.example.crud.domain.product.RequestProduct;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.yaml.snakeyaml.events.Event;
+
+import java.util.Optional;
 
 @RestController //Indicar que essa classe representa um controller
 @RequestMapping("/product") //Definir o caminho desse controller
@@ -26,11 +29,17 @@ public class ProductController {
         repository.save(newProduct);
         return ResponseEntity.ok().build();
     }
-    @PutMapping()
+    @PutMapping
+    @Transactional
     public ResponseEntity updateProduct(@RequestBody @Valid RequestProduct data){ //Método responsável por atualizar
-        Product product = repository.getReferenceById(data.id());// Atualizando por ID
-        product.setName(data.name());
-        product.setPrice_in_cents(data.price_in_cents());
-        return ResponseEntity.ok(product);
+        Optional<Product> optionalProduct = repository.findById(data.id());
+        if(optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            product.setName(data.name());
+            product.setPrice_in_cents(data.price_in_cents());
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
